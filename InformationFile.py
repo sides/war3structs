652d2a3062d425e5ca81b0baec5c6bfb36ef2d39
sides <sides@sides.tv>
2018-11-09 22:40:12 +0000

    InformationFile: Add comments on the "random id" type ids
    It would probably be wise to create new docs as I'm doing all this.
    Oh well... At least it should be easy to do in the future as long as
    all the non-obvious functionality is commented like this.

--- InformationFile.py
+++ InformationFile.py
@@ -104,7 +104,9 @@ RandomUnitTable = Struct(
   "units_count" / Integer,
   "units" / Array(this.units_count, Struct(
     "chance_percent" / Integer,
-    "unit_ids" / Byte[this._.positions_count * 4]
+    "unit_ids" / Byte[this._.positions_count * 4] # this can use the "random id" type of id
+                                                  # see the UnitDoodadRandomUnit struct in
+                                                  # UnitDoodadsFile for details
   ))
 )
 
@@ -116,7 +118,9 @@ RandomItemTable = Struct(
     "items_count" / Integer,
     "items" / Array(this.items_count, Struct(
       "chance_percent" / Integer,
-      "item_id" / Byte[4]
+      "item_id" / Byte[4] # this can use the "random item id" type of id
+                          # see the UnitDoodadRandomUnit struct in
+                          # UnitDoodadsFile for details
     ))
   ))
 )
