a71610f674410a7c831d1c4abdb8fa3e9ed1b1f6
sides <sides@sides.tv>
2018-11-10 15:10:57 +0000

    Add the imports file

---
+++ war3structs/ImportsFile.py
@@ -0,0 +1,20 @@
+from construct import *
+from war3structs.common import *
+
+"""
+  Formats: imp
+  Version: 1
+
+  The imports file contains the import definitions for the map.
+"""
+
+Import = Struct(
+  "is_custom_path" / Byte, # 5 or 8 for no, 10 or 13 for yes
+  "path" / String
+)
+
+ImportsFile = Struct(
+  "version" / Integer,
+  "imports_count" / Integer,
+  "imports" / Array(this.imports_count, Import)
+)
