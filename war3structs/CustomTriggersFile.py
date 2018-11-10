bb46c169a298e96c558bff2bd0b2ca802a9bab70
sides <sides@sides.tv>
2018-11-10 15:03:40 +0000

    Add the custom triggers file

---
+++ war3structs/CustomTriggersFile.py
@@ -0,0 +1,22 @@
+from construct import *
+from war3structs.common import *
+
+"""
+  Formats: wct
+  Version: 1
+
+  The custom triggers file contains JASS code triggers in the map.
+"""
+
+CustomTrigger = Struct(
+  "code_size" / Integer, # including null terminated char
+  "code" / String # is of the length above
+)
+
+CustomTriggersFile = Struct(
+  "version" / Integer,
+  "script_header_comment" / String,
+  "script_header_trigger" / CustomTrigger,
+  "triggers_count" / Integer,
+  "triggers" / Array(this.triggers_count, CustomTrigger)
+)
