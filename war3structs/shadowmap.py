d0bba9d017401fb1438545ad2eb3be70df4fc465
sides <sides@sides.tv>
2018-11-18 21:55:44 +0000

    shadowmap: Flag type already does what we want here

--- war3structs/shadowmap.py
+++ war3structs/shadowmap.py
@@ -8,5 +8,5 @@ from .common import *
 """
 
 ShadowMapFile = Struct(
-  "shadow_map" / GreedyRange(Enum(Byte, TRUE=0xFF, FALSE=0x00)) # the size of the file should be 16*width*height of the tilemap
+  "shadow_map" / GreedyRange(Flag) # the size of the file should be 16*width*height of the tilemap
 )
