978c2d7daa1f7ee6bd991106207c43153b17374a
sides <sides@sides.tv>
2018-11-24 17:18:43 +0000

    jass: Move grammar back to own variable
    It should be clearer what the "struct" here is, which is the grammar,
    so move it to the top where it's immediately seen. I'm still iffy on
    the code below it, since the idea with this repo is to be definitions
    only. Still, it makes more sense than the previous alternative.

--- war3structs/jass.py
+++ war3structs/jass.py
@@ -11,6 +11,169 @@ from lark import Lark, Tree as BaseTree, Token as BaseToken
   similar features to our other constructs.
 """
 
+grammar = """
+  //-------------------------------------------------------------------
+  // Terminals
+  //-------------------------------------------------------------------
+
+  // Keywords
+  CODE        : "code"
+  HANDLE      : "handle"
+  INTEGER     : "integer"
+  REAL        : "real"
+  BOOLEAN     : "boolean"
+  STRING      : "string"
+  ARRAY       : "array"
+  GLOBALS     : "globals"
+  ENDGLOBALS  : "endglobals"
+  CONSTANT    : "constant"
+  NATIVE      : "native"
+  EXTENDS     : "extends"
+  KTYPE       : "type"
+  FUNCTION    : "function"
+  ENDFUNCTION : "endfunction"
+  TAKES       : "takes"
+  RETURNS     : "returns"
+  NOTHING     : "nothing"
+  LOCAL       : "local"
+  RETURN      : "return"
+  SET         : "set"
+  CALL        : "call"
+  LOOP        : "loop"
+  ENDLOOP     : "endloop"
+  EXITWHEN    : "exitwhen"
+  IF          : "if"
+  THEN        : "then"
+  ELSE        : "else"
+  ELSEIF      : "elseif"
+  ENDIF       : "endif"
+  DEBUG       : "debug"
+
+  // Identifier
+  ID      : ("a".."z"|"A".."Z") ("a".."z"|"A".."Z"|"0".."9"|"_")*
+  TYPE    : ID | CODE | HANDLE | INTEGER | REAL | BOOLEAN | STRING
+
+  // Constants
+  INT_CONST_DEC.5 : "0" | "1".."9" ("0".."9")*
+  INT_CONST_OCT.6 : "0" ("0".."7")+
+  INT_CONST_HEX.7 : "$" ("0".."9"|"a".."f"|"A".."F")+ | "0" ("x"|"X") ("0".."9"|"a".."f"|"A".."F")+
+  REAL_CONST.8    : ("0".."9")+ "." ("0".."9")* | "." ("0".."9")+
+  BOOL_CONST.3    : "true" | "false"
+  NULL_CONST.1    : "null"
+
+  // Literals
+  STRING_LITERAL.2 : "\\"" ("\\\\\\""|"\\\\\\\\"|/[^"]/)* "\\""
+  INT_LITERAL.4    : "'" /[^']*/ "'"
+
+  // Operators
+  PLUS    : "+"
+  MINUS   : "-"
+  TIMES   : "*"
+  DIVIDE  : "/"
+  OR      : "or"
+  AND     : "and"
+  NOT     : "not"
+  EQ      : "=="
+  NE      : "!="
+  LT      : "<"
+  GT      : ">"
+  LE      : "<="
+  GE      : ">="
+
+  // Assignment
+  EQUALS : "="
+
+  // Delimiters
+  LPARENS  : "("
+  RPARENS  : ")"
+  LBRACKET : "["
+  RBRACKET : "]"
+  COMMA    : ","
+
+
+  //-------------------------------------------------------------------
+  // Rules
+  //-------------------------------------------------------------------
+
+  start : ((type_declr | globals | native_func_declr) NEWLINE)* (func NEWLINE)*
+
+
+  // Declarations
+
+  type_declr        : KTYPE ID EXTENDS (HANDLE | ID)
+
+  globals           : GLOBALS NEWLINE global_var_declr* ENDGLOBALS
+
+  global_var_declr  : (CONSTANT TYPE ID EQUALS expr NEWLINE | _var_declr NEWLINE)
+
+  native_func_declr : CONSTANT? NATIVE func_declr
+
+  func_declr        : ID TAKES (NOTHING | TYPE ID (COMMA TYPE ID)*) RETURNS (NOTHING | TYPE)
+
+  local_var_declr   : LOCAL _var_declr
+
+  _var_declr        : TYPE ID (EQUALS expr)? | TYPE ARRAY ID
+
+
+  // Functions
+
+  func   : CONSTANT? FUNCTION func_declr NEWLINE locals statements ENDFUNCTION
+
+  locals : (local_var_declr NEWLINE)*
+
+  args   : expr (COMMA expr)*
+
+
+  // Statements
+
+  statements      : (_statement NEWLINE)*
+
+  loop_statements : (_statement NEWLINE | exitwhen NEWLINE)* -> statements
+
+  _statement      : set | call | ifthenelse | loop | return | debug
+
+  set             : SET ID EQUALS expr | SET ID LBRACKET expr RBRACKET EQUALS expr
+
+  call            : CALL func_call
+
+  ifthenelse      : IF expr THEN NEWLINE statements (ELSEIF expr THEN NEWLINE statements)* (ELSE NEWLINE statements)? ENDIF
+
+  loop            : LOOP NEWLINE loop_statements ENDLOOP
+
+  exitwhen        : EXITWHEN expr
+
+  return          : RETURN expr?
+
+  debug           : DEBUG (set | call | ifthenelse | loop)
+
+
+  // Expressions
+
+  expr           : binary_op | unary_op | func_call | array_ref | func_ref | ID | _const | parens
+
+  binary_op      : expr (PLUS | MINUS | TIMES | DIVIDE | EQ | NE | LT | GT | LE | GE | AND | OR) expr
+
+  unary_op       : (PLUS | MINUS | NOT) expr
+
+  func_call      : ID LPARENS args? RPARENS
+
+  array_ref      : ID LBRACKET expr RBRACKET
+
+  func_ref       : FUNCTION ID
+
+  _const         : REAL_CONST | INT_CONST_HEX | INT_CONST_OCT | INT_CONST_DEC | INT_LITERAL | BOOL_CONST | STRING_LITERAL | NULL_CONST
+
+  parens         : LPARENS expr RPARENS
+
+
+  NEWLINE    : /([\\r]?[\\n])+/
+  WHITESPACE : /[ \\t\\f\\r\\n]+/
+  COMMENT    : /\\/\\/[^\\n]*/
+
+  %ignore COMMENT
+  %ignore WHITESPACE
+"""
+
 class Tree(BaseTree):
   pass
 
@@ -18,169 +181,6 @@ class Token(BaseToken):
   pass
 
 class JassParser():
-  _grammar = """
-    //-------------------------------------------------------------------
-    // Terminals
-    //-------------------------------------------------------------------
-
-    // Keywords
-    CODE        : "code"
-    HANDLE      : "handle"
-    INTEGER     : "integer"
-    REAL        : "real"
-    BOOLEAN     : "boolean"
-    STRING      : "string"
-    ARRAY       : "array"
-    GLOBALS     : "globals"
-    ENDGLOBALS  : "endglobals"
-    CONSTANT    : "constant"
-    NATIVE      : "native"
-    EXTENDS     : "extends"
-    KTYPE       : "type"
-    FUNCTION    : "function"
-    ENDFUNCTION : "endfunction"
-    TAKES       : "takes"
-    RETURNS     : "returns"
-    NOTHING     : "nothing"
-    LOCAL       : "local"
-    RETURN      : "return"
-    SET         : "set"
-    CALL        : "call"
-    LOOP        : "loop"
-    ENDLOOP     : "endloop"
-    EXITWHEN    : "exitwhen"
-    IF          : "if"
-    THEN        : "then"
-    ELSE        : "else"
-    ELSEIF      : "elseif"
-    ENDIF       : "endif"
-    DEBUG       : "debug"
-
-    // Identifier
-    ID      : ("a".."z"|"A".."Z") ("a".."z"|"A".."Z"|"0".."9"|"_")*
-    TYPE    : ID | CODE | HANDLE | INTEGER | REAL | BOOLEAN | STRING
-
-    // Constants
-    INT_CONST_DEC.5 : "0" | "1".."9" ("0".."9")*
-    INT_CONST_OCT.6 : "0" ("0".."7")+
-    INT_CONST_HEX.7 : "$" ("0".."9"|"a".."f"|"A".."F")+ | "0" ("x"|"X") ("0".."9"|"a".."f"|"A".."F")+
-    REAL_CONST.8    : ("0".."9")+ "." ("0".."9")* | "." ("0".."9")+
-    BOOL_CONST.3    : "true" | "false"
-    NULL_CONST.1    : "null"
-
-    // Literals
-    STRING_LITERAL.2 : "\\"" ("\\\\\\""|"\\\\\\\\"|/[^"]/)* "\\""
-    INT_LITERAL.4    : "'" /[^']*/ "'"
-
-    // Operators
-    PLUS    : "+"
-    MINUS   : "-"
-    TIMES   : "*"
-    DIVIDE  : "/"
-    OR      : "or"
-    AND     : "and"
-    NOT     : "not"
-    EQ      : "=="
-    NE      : "!="
-    LT      : "<"
-    GT      : ">"
-    LE      : "<="
-    GE      : ">="
-
-    // Assignment
-    EQUALS : "="
-
-    // Delimiters
-    LPARENS  : "("
-    RPARENS  : ")"
-    LBRACKET : "["
-    RBRACKET : "]"
-    COMMA    : ","
-
-
-    //-------------------------------------------------------------------
-    // Rules
-    //-------------------------------------------------------------------
-
-    start : ((type_declr | globals | native_func_declr) NEWLINE)* (func NEWLINE)*
-
-
-    // Declarations
-
-    type_declr        : KTYPE ID EXTENDS (HANDLE | ID)
-
-    globals           : GLOBALS NEWLINE global_var_declr* ENDGLOBALS
-
-    global_var_declr  : (CONSTANT TYPE ID EQUALS expr NEWLINE | _var_declr NEWLINE)
-
-    native_func_declr : CONSTANT? NATIVE func_declr
-
-    func_declr        : ID TAKES (NOTHING | TYPE ID (COMMA TYPE ID)*) RETURNS (NOTHING | TYPE)
-
-    local_var_declr   : LOCAL _var_declr
-
-    _var_declr        : TYPE ID (EQUALS expr)? | TYPE ARRAY ID
-
-
-    // Functions
-
-    func   : CONSTANT? FUNCTION func_declr NEWLINE locals statements ENDFUNCTION
-
-    locals : (local_var_declr NEWLINE)*
-
-    args   : expr (COMMA expr)*
-
-
-    // Statements
-
-    statements      : (_statement NEWLINE)*
-
-    loop_statements : (_statement NEWLINE | exitwhen NEWLINE)* -> statements
-
-    _statement      : set | call | ifthenelse | loop | return | debug
-
-    set             : SET ID EQUALS expr | SET ID LBRACKET expr RBRACKET EQUALS expr
-
-    call            : CALL func_call
-
-    ifthenelse      : IF expr THEN NEWLINE statements (ELSEIF expr THEN NEWLINE statements)* (ELSE NEWLINE statements)? ENDIF
-
-    loop            : LOOP NEWLINE loop_statements ENDLOOP
-
-    exitwhen        : EXITWHEN expr
-
-    return          : RETURN expr?
-
-    debug           : DEBUG (set | call | ifthenelse | loop)
-
-
-    // Expressions
-
-    expr           : binary_op | unary_op | func_call | array_ref | func_ref | ID | _const | parens
-
-    binary_op      : expr (PLUS | MINUS | TIMES | DIVIDE | EQ | NE | LT | GT | LE | GE | AND | OR) expr
-
-    unary_op       : (PLUS | MINUS | NOT) expr
-
-    func_call      : ID LPARENS args? RPARENS
-
-    array_ref      : ID LBRACKET expr RBRACKET
-
-    func_ref       : FUNCTION ID
-
-    _const         : REAL_CONST | INT_CONST_HEX | INT_CONST_OCT | INT_CONST_DEC | INT_LITERAL | BOOL_CONST | STRING_LITERAL | NULL_CONST
-
-    parens         : LPARENS expr RPARENS
-
-
-    NEWLINE    : /([\\r]?[\\n])+/
-    WHITESPACE : /[ \\t\\f\\r\\n]+/
-    COMMENT    : /\\/\\/[^\\n]*/
-
-    %ignore COMMENT
-    %ignore WHITESPACE
-  """
-
   _build_space_before = [
     'TAKES', 'RETURNS',
     'THEN',
@@ -199,7 +199,7 @@ class JassParser():
 
   def _get_lark():
     if JassParser._lark is None:
-      JassParser._lark = Lark(JassParser._grammar, parser="lalr", tree_class=Tree)
+      JassParser._lark = Lark(grammar, parser="lalr", tree_class=Tree)
 
     return JassParser._lark
 
