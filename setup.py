e6d18ddacfb9cedeb74878f7c7b7de0f71d31b99
sides <sides@sides.tv>
2019-01-01 20:29:28 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     'lark-parser>=0.6'
   ],
   name="war3structs",
-  version="0.1.0",
+  version="0.1.1",
   author="sides",
   author_email="sides@sides.tv",
   description="Python construct definitions for Warcraft III file formats",
